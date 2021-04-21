812637702a8509a5f9be7c804649b9479a0e7bf1
unknown <you@example.com>
2021-04-21 15:11:06 +0530

    react

---
+++ React-main/studentapp/src/Components/About.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import  class About extends Component
+{
+    render()
+    {
+        <div>
+        <h3>Welcome to the About Page of Student Management Portal</h3>
+        </div>
+    }
+}
---
+++ React-main/studentapp/src/Components/Contact.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+ import class Contact extends Component
+{
+    render()
+    {
+        <div>
+        <h3>Welcome to the Contact Page of Student Management Portal</h3>
+        </div>
+    }
+}
